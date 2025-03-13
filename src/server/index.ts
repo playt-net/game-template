5b1f67d05ccab9c4928661c0d269ef1253fc5201
Philipp Gartz <philipp@gartz.dev>
2025-03-13 14:11:13 +0100

    Add build

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,23 @@
 import { Elysia } from "elysia";
 import matchRouter from "./router/match";
 import gameRouter from "./router/game";
+import { staticPlugin } from "@elysiajs/static";
 
-const app = new Elysia().use(matchRouter).use(gameRouter).listen(3000);
+const isDev = process.env.NODE_ENV === "development";
+
+const app = new Elysia().use(matchRouter).use(gameRouter);
+
+if (!isDev) {
+	app.use(
+		staticPlugin({
+			assets: "dist",
+			prefix: "/",
+			ignorePatterns: ["/images/"],
+		}),
+	);
+}
+
+app.listen(3000);
 
 console.log(
 	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
